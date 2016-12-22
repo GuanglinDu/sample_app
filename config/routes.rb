7359d9fffac8f7d7caa96f4fedeb4f49a3d1204c
Guanglin Du <guanglin.du@gmail.com>
2016-12-22 16:20:33 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # For details on the DSL available within this file,
 # see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  root 'static_pages#home'
 end

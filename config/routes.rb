63a0a40e897002e0b34ff48fe92b1dcc58de2d59
Guanglin Du <guanglin.du@gmail.com>
2017-02-22 17:09:45 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 # see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  get '/signup',  to: 'users#new'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 end
